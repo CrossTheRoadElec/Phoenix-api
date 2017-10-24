ba8d88666085c71d737d718e6197593c882fc027
Omar Zrien <Ozrien@crosstheroadelectronics.com>
2017-10-24 18:16:12 -0400

    eclipse project/batch fix for new names/paths

--- Eclipse_DeployForDebug.bat
+++ Eclipse_DeployForDebug.bat
@@ -1,5 +1,5 @@
 :: move the headers into the WPILIB folder for FRC applications
-xcopy /Y /E ..\..\CTRLib\cpp\include\ctrlib %HOMEPATH%\wpilib\user\cpp\include\ctrlib
+xcopy /Y /E ..\..\Phoenix-frc-lib\cpp\include\ctre %HOMEPATH%\wpilib\user\cpp\include\ctre
 
 :: move the low level static lib so Robot apps use it
-xcopy /Y /E .\libCTRLib.a %HOMEPATH%\wpilib\user\cpp\lib\libCTRLib.a
+xcopy /Y /E .\libCTRE_Phoenix.a %HOMEPATH%\wpilib\user\cpp\lib\libCTRE_Phoenix.a
