0313b8a0f885008a9c46accdbcb1cc82e107b341
Omar Zrien <Ozrien@crosstheroadelectronics.com>
2017-10-30 13:09:55 -0400

    eclipse bat can be run from any directory context.

--- Eclipse_DeployForDebug.bat
+++ Eclipse_DeployForDebug.bat
@@ -1,3 +1,6 @@
+:: set working directory to where the compiled lib is
+cd %~dp0\Debug
+
 :: move the headers into the WPILIB folder for FRC applications
 xcopy /Y /E ..\..\Phoenix-frc-lib\cpp\include\ctre %HOMEPATH%\wpilib\user\cpp\include\ctre
 
