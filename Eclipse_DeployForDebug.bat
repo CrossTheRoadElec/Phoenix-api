7884533fe3cbdb3f8d9b1223b5fb8eec3208b04b
Omar Zrien <Ozrien@crosstheroadelectronics.com>
2017-11-20 21:05:55 -0500

    batch fix to remove windows file-vs-folder-prompt.

--- Eclipse_DeployForDebug.bat
+++ Eclipse_DeployForDebug.bat
@@ -8,4 +8,4 @@ xcopy /Y /E ..\..\Phoenix-frc-lib\cpp\include\ctre %HOMEPATH%\wpilib\user\cpp\in
 xcopy /Y /E ..\..\Phoenix-frc-lib\libraries\driver\include\ctre %HOMEPATH%\wpilib\user\cpp\include\ctre
 
 :: move the low level static lib so Robot apps use it
-xcopy /Y /E .\libCTRE_Phoenix.a %HOMEPATH%\wpilib\user\cpp\lib\libCTRE_Phoenix.a
+echo F|xcopy /Y /E .\libCTRE_Phoenix.a %HOMEPATH%\wpilib\user\cpp\lib\libCTRE_Phoenix.a
