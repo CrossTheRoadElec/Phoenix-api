cf4a97917c70fd04827347b4771e5a65a3d61417
Omar Zrien <Ozrien@crosstheroadelectronics.com>
2017-12-11 00:28:38 -0500

    added to global header, alphabetized it.

--- cpp/include/ctre/Phoenix.h
+++ cpp/include/ctre/Phoenix.h
@@ -1,22 +1,25 @@
 #pragma once
 #include "ctre/phoenix/CANifier.h"
+#include "ctre/phoenix/HsvToRgb.h"
+#include "ctre/phoenix/LinearInterpolation.h"
+#include "ctre/phoenix/Motion/MotionProfileStatus.h"
+#include "ctre/phoenix/Motion/TrajectoryPoint.h"
 #include "ctre/phoenix/MotorControl/CAN/TalonSRX.h"
 #include "ctre/phoenix/MotorControl/CAN/VictorSPX.h"
 #include "ctre/phoenix/MotorControl/Faults.h"
 #include "ctre/phoenix/MotorControl/SensorCollection.h"
+#include "ctre/phoenix/MotorControl/IMotorController.h"
+#include "ctre/phoenix/MotorControl/IMotorControllerEnhanced.h"
 #include "ctre/phoenix/Sensors/PigeonIMU.h"
+#include "ctre/phoenix/Signals/MovingAverage.h"
 #include "ctre/phoenix/Tasking/Schedulers/ConcurrentScheduler.h"
 #include "ctre/phoenix/Tasking/ILoopable.h"
 #include "ctre/phoenix/Tasking/IProcessable.h"
 #include "ctre/phoenix/Tasking/ButtonMonitor.h"
-#include "ctre/phoenix/LinearInterpolation.h"
-#include "ctre/phoenix/HsvToRgb.h"
-#include "ctre/phoenix/MotorControl/IMotorController.h"
-#include "ctre/phoenix/MotorControl/IMotorControllerEnhanced.h"
-#include "ctre/phoenix/Signals/MovingAverage.h"
 
 using namespace ctre;
 using namespace ctre::phoenix;
+using namespace ctre::phoenix::motion;
 using namespace ctre::phoenix::motorcontrol;
 using namespace ctre::phoenix::motorcontrol::can;
 using namespace ctre::phoenix::sensors;
