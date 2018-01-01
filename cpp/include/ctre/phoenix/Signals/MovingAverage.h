4a60fb2bdff379eec2d3735e184676f6bdc4f4f1
Omar Zrien <Ozrien@crosstheroadelectronics.com>
2018-01-01 18:47:54 -0500

    typos/spelling fixes/grammer fixes

--- cpp/include/ctre/phoenix/Signals/MovingAverage.h
+++ cpp/include/ctre/phoenix/Signals/MovingAverage.h
@@ -1,23 +1,13 @@
-/*********************************************************************
- * Software License Agreement
+/*
+ *  Software License Agreement
  *
- * Copyright (C) 2016 Cross The Road Electronics.  All rights
+ * Copyright (C) Cross The Road Electronics.  All rights
  * reserved.
- *
+ * 
  * Cross The Road Electronics (CTRE) licenses to you the right to 
- * compile and modify the following source for the sole purpose of  
- * expanding the feature set of the Hydro-Gear Etesia demo platform.
- * This is considered a derivative work that cannot be published,
- * distributed, sublicensed, or sold.
- *
- * CTRE does not license to you the right to use or modify in any 
- * other circumstance.
- * CTRE does not license to you the right to publish, 
- * distribute, sublicense, or sell copies in any circumstance.
- *
- * The above copyright notice and this permission notice shall be included 
- * in all copies or substantial portions of the Software.
- *
+ * use, publish, and distribute copies of CRF (Cross The Road) firmware files (*.crf) and Software
+ * API Libraries ONLY when in use with Cross The Road Electronics hardware products.
+ * 
  * THE SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT
  * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
  * LIMITATION, ANY WARRANTY OF MERCHANTABILITY, FITNESS FOR A
@@ -28,9 +18,8 @@
  * BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY DEFENSE
  * THEREOF), ANY CLAIMS FOR INDEMNITY OR CONTRIBUTION, OR OTHER
  * SIMILAR COSTS, WHETHER ASSERTED ON THE BASIS OF CONTRACT, TORT
- * (INCLUDING NEGLIGENCE), BREACH OF WARRANTY, OR OTHERWISE.
- *
- ********************************************************************/
+ * (INCLUDING NEGLIGENCE), BREACH OF WARRANTY, OR OTHERWISE
+ */
 
 namespace ctre {
 namespace phoenix {
